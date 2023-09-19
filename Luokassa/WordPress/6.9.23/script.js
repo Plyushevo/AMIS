2f8aa345e8931e06731964cad7a19d4a0b5112e8
Кирилл Бельков <plyushevo@gmail.com>
2023-09-19 16:55:16 +0300

    refactor folders

--- Luokassa/WordPress/6.9.23/script.js
+++
@@ -1,3 +0,0 @@
-const allCandidates = document.querySelectorAll('input')
-const voteCandidatesButtons = document.getElementById['1','2','3','4','5','6']
-console.log(allCandidates, voteCandidatesButtons)
